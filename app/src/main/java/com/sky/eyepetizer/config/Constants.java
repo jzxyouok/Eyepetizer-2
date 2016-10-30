7879dfbd7027d2d031018f54aced0e500ce7d0fa
tonycheng <bc1341286335@gmail.com>
2016-10-30 16:04:15 +0800

    first commit

---
+++ app/src/main/java/com/sky/eyepetizer/config/Constants.java
@@ -0,0 +1,17 @@
+package com.sky.eyepetizer.config;
+
+/**
+ * 项目名称：Eyepetizer
+ * 类描述：
+ * 创建人：tonycheng
+ * 创建时间：2016/10/30 15:43
+ * 邮箱：tonycheng93@outlook.com
+ * 修改人：
+ * 修改时间：
+ * 修改备注：
+ */
+
+public class Constants {
+
+    public static final String BASE_URL = "";
+}
